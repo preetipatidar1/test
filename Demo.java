11d3943fb737e68f577169e4013a359501ecd86e
preetipatidar1 <69299139+preetipatidar1@users.noreply.github.com>
2020-08-09 00:04:37 -0700

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+public static void main(String []args)
+{
+System.out.println("hello world");
+System.out.println("first line");
+System.out.println("second line");
+System.out.println("third line");
+}
+}
